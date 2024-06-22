59e883d16faad9329a326929343a3a8d18add5a6
maciejbr <m.broniarski@gmail.com>
2024-06-22 22:09:21 +0200

    Update first.js

--- js/first.js
+++ js/first.js
@@ -1,11 +1,1 @@
 "use strict";
-
-// Deklaracja parametrów x, y, z
-function multiply(x, y, z) {
-  console.log(`Result: ${x * y * z}`);
-}
-
-// Przekazywanie argumentów
-multiply(2, 3, 5); // "Result: 30"
-multiply(4, 8, 12); // "Result: 384"
-multiply(17, 6, 25); // "Result: 2550"
