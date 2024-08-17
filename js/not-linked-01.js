128e65d05312b7101eeaa343e399fb9895fd4a05
maciejbr <m.broniarski@gmail.com>
2024-08-17 18:42:29 +0200

    Update not-linked-01.js

--- js/not-linked-01.js
+++ js/not-linked-01.js
@@ -133,6 +133,7 @@ console.log((0.1 * 10 + 0.2 * 10) / 10); // 0.3
 console.log((0.1 + 0.2).toFixed(1)); // "0.3"
 console.log((5).toFixed(2)); //  "5.00"
 console.log((8.762195).toFixed(4)); //  "8.7622"
+console.log((0.1 + 0.2).toFixed()); // "0"
 
 // Funkcje
 
