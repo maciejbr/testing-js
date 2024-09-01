ecd50f92d8c30202d78ef911bc27318e7d3bfda9
maciejbr <m.broniarski@gmail.com>
2024-09-01 21:42:30 +0200

    Update not-linked-07.js

--- js/not-linked-07.js
+++ js/not-linked-07.js
@@ -253,3 +253,271 @@ list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
 // Metoda addEventListener()
 
 element.addEventListener(zdarzenie, obsługa, opcje)
+
+<button class="my-button">Następny</button>
+
+const button = document.querySelector(".my-button");
+
+const handleClick = () => {
+  console.log("Przycisk został wciśnięty, teraz pojawi się następny obraz");
+};
+
+button.addEventListener("click", handleClick);
+
+// z CodePen
+
+const singleBtn = document.querySelector("#single");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+singleBtn.addEventListener("click", handleClick);
+
+// ===============================================
+const multiBtn = document.querySelector("#multiple");
+
+const firstCallback = () => {
+  console.log("First callback!");
+};
+const secondCallback = () => {
+  console.log("Second callback!");
+};
+const thirdCallback = () => {
+  console.log("Third callback!");
+};
+
+multiBtn.addEventListener("click", firstCallback);
+multiBtn.addEventListener("click", secondCallback);
+multiBtn.addEventListener("click", thirdCallback);
+
+// Metoda removeEventListener()
+
+element.removeEventListener(zdarzenie, obsługa, opcje)
+
+
+const button = document.querySelector(".my-button");
+
+const handleClick = () => {
+  console.log("Przycisk został wciśnięty, teraz pojawi się następny obraz");
+};
+
+button.addEventListener("click", handleClick);
+
+// z CodePen:
+const addListenerBtn = document.querySelector('.js-add');
+const removeListenerBtn = document.querySelector('.js-remove');
+const btn = document.querySelector(".target-btn");
+
+const handleClick = () => {
+  console.log("click event listener callback");
+};
+
+addListenerBtn.addEventListener("click", () => {
+  btn.addEventListener("click", handleClick);
+  console.log("click event listener was added to btn");
+});
+
+removeListenerBtn.addEventListener("click", () => {
+  btn.removeEventListener("click", handleClick);
+  console.log("click event listener was removed from btn");
+});
+
+// Obiekt zdarzenia Event
+
+const handleClick = event => {
+  console.log(event);
+};
+
+button.addEventListener("click", handleClick);
+
+// z codePen:
+
+const button = document.querySelector(".btn");
+
+const handleClick = (event) => {
+  console.log("event: ", event);
+  console.log("event type: ", event.type);
+  console.log("currentTarget: ", event.currentTarget);
+};
+
+button.addEventListener("click", handleClick);
+
+// Zdarzenia klawiatury
+
+// W przeciwieństwie do innych zdarzeń, zdarzenia klawiatury są obsługiwane
+// na dokumencie, a nie na konkretnym elemencie.Obiekty zdarzeń klawiatury 
+// dziedziczą po bazowej klasie KeyboardEvent.
+
+document.addEventListener("keydown", event => {
+  console.log("Naciśnięcie klawisza: ", event);
+});
+
+document.addEventListener("keyup", event => {
+  console.log("Zwolnienie klawisza: ", event);
+});
+
+// Właściwości key i code
+
+// Właściwość obiektu zdarzenia key zwraca symbol wygenerowany przez naciśnięcie 
+// klawisza na klawiaturze.Ta właściwość uwzględnia:
+// - stan klawiszy modyfikujących, na przykład Shift
+// - bieżący język
+
+// Właściwość obiektu zdarzenia code zwraca kod fizycznego klawisza na klawiaturze 
+// i nie zależy od języka oraz stanu klawiszy modyfikujących.
+
+document.addEventListener("keydown", event => {
+  console.log("key: ", event.key);
+  console.log("code: ", event.code);
+});
+
+// z CodePen
+
+HTML  CSS  Babel Result  
+EDIT ON
+
+ const clearLogBtn = document.querySelector(".js-clear");
+const logList = document.querySelector(".log-list");
+let keypressCounter = 1;
+
+console.log(clearLogBtn)
+
+document.addEventListener("keydown", logMessage);
+document.addEventListener("keyup", logMessage);
+clearLogBtn.addEventListener("click", reset);
+
+function logMessage({ type, key, code }) {
+  const markup = `<div class="log-item">
+    <span class="chip">${keypressCounter}</span>
+    <ul>
+      <li><b>Event</b>: ${type}</li>
+      <li><b>Key</b>: ${key}</li>
+      <li><b>Code</b>: ${code}</li>
+    </ul>
+  </div>`;
+
+  logList.insertAdjacentHTML("afterbegin", markup);
+
+  if (type === "keyup") {
+    incrementKeypressCounter();
+  }
+}
+
+function reset() {
+  keypressCounter = 1;
+  logList.innerHTML = "";
+}
+
+function incrementKeypressCounter() {
+  keypressCounter += 1;
+}
+
+// Klawisze modyfikatorów
+
+document.addEventListener("keydown", event => {
+  if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
+    console.log("Kombinacja «Ctrl + s» lub «Command + s»");
+  }
+});
+
+// Ostatnio do obsługi kombinacji klawiszy używano właściwości keyCode.
+// Wiele wpisów na forach i blogach może nadal z niej korzystać.
+// Pamiętaj jednak, że keyCode jest już przestarzała, a zamiast niej 
+// warto używać właściwości key i code.
+
+//----- Zdarzenia elementów formularza ------
+
+// Zdarzenie submit
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+	// ...
+});
+
+// Aby anulować domyślną akcję przeglądarki, używamy standardowej metody
+// obiektu zdarzenia preventDefault().
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", event => {
+	event.preventDefault();
+});
+
+// Zdarzenie submit można wykorzystać do walidacji formularza przed wysłaniem,
+// ponieważ obiekt zdarzenia zawiera wiele przydatnych właściwości związanych 
+// z elementami formularza.
+
+// z CodePen
+const registerForm = document.querySelector(".form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  
+  if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
+
+// Właściwość elements obiektu DOM formularza zawiera obiekt z odnośnikami do wszystkich jego elementów, które posiadają atrybut name. Dlatego w przykładzie uzyskujemy wartości pól, odwołując się do event.target.elements.login.value i event.target.elements.password.value.
+
+// Zdarzenie change
+
+// z CodePen
+const select = document.querySelector(".pizza-select");
+const textOutput = document.querySelector(".text-output");
+const valueOutput = document.querySelector(".value-output");
+
+select.addEventListener("change", setOutput);
+
+function setOutput(event) {
+  const selectedOptionValue = event.currentTarget.value;
+  const selectedOptionIndex = event.currentTarget.selectedIndex;
+  const selectedOptionText =
+    event.currentTarget.options[selectedOptionIndex].text;
+
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
+}
+
+// Zdarzenie input
+// z CodePen
+
+const textInput = document.querySelector(".text-input");
+const output = document.querySelector(".output");
+
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
+
+// Zdarzenie focus i blur
+// z CodePen
+
+const textInput = document.querySelector(".text-input");
+const setFocusBtn = document.querySelector('[data-action="set"]');
+const removeFocusBtn = document.querySelector('[data-action="remove"]');
+
+setFocusBtn.addEventListener("click", () => {
+  textInput.focus();
+});
+
+removeFocusBtn.addEventListener("click", () => {
+  textInput.blur();
+});
+
+textInput.addEventListener("focus", () => {
+  textInput.value = "This input has focus";
+});
+
+textInput.addEventListener("blur", () => {
+  textInput.value = "";
+});
