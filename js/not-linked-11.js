fa822312d2575850f992f4b755561a723c90a2a7
maciejbr <m.broniarski@gmail.com>
2024-09-09 21:00:40 +0200

    Update not-linked-11.js

--- js/not-linked-11.js
+++ js/not-linked-11.js
@@ -177,3 +177,130 @@ Access-Control-Allow-Origin: *
 //------- AJAX -------
 
 // AJAX (Asynchronous JavaScript and XML) to metoda odbierania lub wysyłania danych, a następnie aktualizowania interfejsu na podstawie tych danych, bez konieczności przeładowywania strony. Skraca to czas odpowiedzi i sprawia, że strona internetowa jest bardziej interaktywna.
+
+// AJAX interpretuje się jako dowolną komunikację z serwerem bez przeładowywania strony.
+
+<button type="button" class="btn">Fetch users</button>
+<ul class="user-list"></ul>
+
+// Po kliknięciu przycisku Fetch users wykonujemy żądanie HTTP używając znanej nam już metody fetch.
+
+const fetchUsersBtn = document.querySelector(".btn");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetch("<https://jsonplaceholder.typicode.com/users>")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((users) => {
+			// Dane z backendu
+			console.log(users);
+		})
+    .catch((error) => console.log(error));
+});
+
+// W drugim bloku then uzyskujemy dostęp do danych odpowiedzi z backendu. To właśnie tutaj można wykorzystać te dane do aktualizacji interfejsu, tworzenia znaczników HTML i dodawania ich do DOM do istniejącego elementu.
+
+// Do tworzenia znaczników HTML można użyć dowolnej wygodnej metody, takiej jak createElement lub insertAdjacentHTML. Przyjrzyjmy się jednej ze standardowych technik.
+
+// Iterujemy po tablicy obiektów przy użyciu metody map
+// W każdej iteracji zwracamy znaczniki HTML dla jednego elementu kolekcji
+// Wynikową tablicę ciągów zszywamy w jeden ciąg przy użyciu metody join("")
+// Dodajemy wiersz ze znacznikami HTML do DOM za pomocą metody insertAdjacentHTML
+
+const fetchUsersBtn = document.querySelector(".btn");
+const userList = document.querySelector(".user-list");
+
+fetchUsersBtn.addEventListener("click", () => {
+  fetch("<https://jsonplaceholder.typicode.com/users>")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then((users) => {
+      const markup = users.map((user) => {
+				return `<li>
+	          <p><b>Name</b>: ${user.name}</p>
+	          <p><b>Email</b>: ${user.email}</p>
+	          <p><b>Company</b>: ${user.company.name}</p>
+	        </li>`;
+        })
+        .join("");
+
+			userList.insertAdjacentHTML("beforeend", markup);
+    })
+    .catch((error) => console.log(error));
+});
+
+// Przeanalizuj żywy przykład kodu. Zauważ, jak zrefaktoryzowaliśmy kod, aby był bardziej czytelny.
+
+// Przenieśliśmy kod żądania do funkcji fetchUsers(), która wykonuje żądanie HTTP i zwraca wynik.
+// W obsłudze zdarzenia kliknięcia wywołujemy ją i dodajemy łańcuch metod then() i catch() do wyniku jej wywołania.
+// Przenieśliśmy kod do tworzenia i dodawania znaczników HTML do funkcji renderUsers(users), która oczekuje tablicy użytkowników.
+
+//
+
+//------- Błąd początkującego -------
+
+// Przeanalizujmy powszechny błąd popełniany podczas pracy z kodem asynchronicznym — próbę użycia danych żądania HTTP poza callbackiem metody then() w kodzie synchronicznym.
+
+// Początkujący próbuje zapisać „wynik fetch" do zewnętrznej zmiennej i użyć go poniżej w kodzie synchronicznym natychmiast po wywołaniu metody fetch().
+
+let globalVariable; // undefined
+
+// Wykonanie żądania HTTP
+fetch("<https://jsonplaceholder.typicode.com/users>")
+  .then(response => response.json())
+  .then(users => {
+    console.log("users inside then callback: ", users);
+
+    // Asynchroniczne zapisywanie wyniku zapytania do zewnętrznej zmiennej
+    globalVariable = users;
+
+    // W środku callback metody then wszystko jest w porządku
+    console.log("globalVariable inside fetch callback: ", globalVariable);
+  });
+
+// Z zewnątrz callbacku metody then w kodzie synchronicznym nie ma dostępu do danych asynchronicznej odpowiedzi backendu
+console.log("globalVariable outside fetch: ", globalVariable); // undefined
+
+// W naszym powyższym przykładzie poza callbackiem metody then() nie ma żadnych danych. Ponieważ ostatni console.log() zostanie wykonany wcześniej (kod synchroniczny), niż nastąpi odpowiedź serwera (kod asynchroniczny). W momencie wywołania console.log("globalVariable outside fetch: ", globalVariable) zmienna globalVariable nadal ma wartość undefined. Wartość obietnicy (promise), która została wykonana, jest dostępna tylko w kodzie asynchronicznym, w naszym przykładzie w callbacku metody then().
+
+// Dlatego bardzo ważne jest, aby pamiętać, że próba zapisania wyniku operacji asynchronicznej do zmiennej zewnętrznej i wykorzystania go w kodzie synchronicznym jest nieefektywnym podejściem. Zamiast tego lepiej jest używać metod .then() i .catch() do przetwarzania wyników operacji asynchronicznych natychmiast po ich otrzymaniu.
+
+// Czy możliwe jest uzyskanie dostępu do danych żądania HTTP poza metodą then w kodzie synchronicznym?
+// NIE, te dane są wartością obietnicy (promise), która jest przetwarzana asynchronicznie przez metodę then.
+
+// Parametry ciągu żądania
+// Parametry żądania pozwalają określić dodatkowe kryteria dla backendu
+// Symbol ? oznacza początek parametrów żądania. Każdy parametr jest parą — nazwa=wartość. W ciągu parametry są oddzielone symbolem &.
+
+// Dokumentacja JSONPlaceholder API opisuje możliwe parametry ciągu żądania:
+// _limit, który kontroluje liczbę elementów w odpowiedzi;
+// _sort, który odpowiada za kolejność sortowania obiektów w tablicy.
+
+fetch("<https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name>")
+
+// Żądanie GET w powyższym przykładzie zwróci tablicę zawierającą siedmiu użytkowników (łącznie 10) posortowanych alfabetycznie według nazwy (pole name). Podkreślenia w nazwach parametrów są specyficzne dla tego backendu, nie jest to standard.
+
+// Klasa URLSearchParams
+
+// Parametrów może być wiele i niewygodne jest łączenie ich w jeden długi ciąg, zarówno dla czytelności, jak i dalszej edycji.
+
+// Podczas komponowania ciągów parametrów tworzona jest instancja klasy URLSearchParams, która jest inicjowana obiektem. Wynikiem będzie specjalny obiekt (iterator) z metodami. W przypadku konwertowania ciągów zwraca on wynik wywołania metody toString() — swoje odwzorowanie ciągu.
+
+const searchParams = new URLSearchParams({
+  _limit: 5,
+  _sort: "name",
+});
+
+console.log(searchParams.toString()); // "_limit=5&_sort=name"
+const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+console.log(url); // "<https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name>"
+
+// W postaci ciągu znaków właściwości obiektu staną się parametrami i ich wartościami. Parametry będą oddzielone znakiem &. Podczas interpolacji wartości w szablonach ciągów znaków, zostaje niejawnie przekonwertowana na ciąg znaków. Więc nie ma potrzeby wywoływania metody toString() podczas tworzenia adresu URL. Nie zapomnij rozpocząć ciąg od znaku ?.
